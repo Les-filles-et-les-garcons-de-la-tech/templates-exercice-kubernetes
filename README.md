909711c191c2bac5b5cac2f7e59d62af96e70b74
herve <rvfontbonne@gmail.com>
2023-12-09 17:10:00 +0100

    - change for domain / add OPA demo

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ Dans TOUT le dossier :
 Avec un éditeur de texte ou le script `replace_variables.sh` à renseigner.
 
 - Remplacer `_NUMBER_` par le **nombre** attribué par le formateur (**01**-15).
-- Remplacer `_IP_LB_INGR_CONTR_` par l'adresse du Load Balancer du service de l'`ingress controller`
-- Remplacer `_PROTOCOLE_` par http ou https
-- **OPTIONNEL** : Remplacer `_IP_NODE_` par l'adresse (IP) d'un Node Worker.
+- Remplacer `_DOMAIN_NAME_` par **formation.fgtech.fr**
+- Remplacer `_PROTOCOLE_` par **https**
+- **OPTIONNEL** : Remplacer `_IP_NODE_` par l'adresse (IP) d'un Node Worker (plus tard)
 
 
--- replace_variables.sh
+++ replace_variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 NUMBER=01
-IP_LB_INGR_CONTR=formation.fgtech.fr
+DOMAIN_NAME=formation.fgtech.fr
 PROTOCOLE=https
 IP_NODE=10.10.11.110
 EXTERNAL_IP_SVC_BACKEND=_EXTERNAL_IP_SVC_BACKEND_
@@ -10,7 +10,7 @@ EXTERNAL_IP_SVC_BACKEND=_EXTERNAL_IP_SVC_BACKEND_
 find . -type f -name "*.yaml" | xargs sed -i  "s/_NUMBER_/$NUMBER/g;"
 find . -type f -name "*.yaml" | xargs sed -i  "s/_IP_NODE_/$IP_NODE/g;"
 find . -type f -name "*.yaml" | xargs sed -i  "s/_EXTERNAL_IP_SVC_BACKEND_/$EXTERNAL_IP_SVC_BACKEND/g;"
-find . -type f -name "*.yaml" | xargs sed -i  "s/_IP_LB_INGR_CONTR_/$IP_LB_INGR_CONTR/g;"
+find . -type f -name "*.yaml" | xargs sed -i  "s/_DOMAIN_NAME_/$DOMAIN_NAME/g;"
 find . -type f -name "*.yaml" | xargs sed -i  "s/_PROTOCOLE_/$PROTOCOLE/g;"
 
 find 15-rbac -type f -name "*.sh" | xargs sed -i  "s/_NUMBER_/$NUMBER/g;"
