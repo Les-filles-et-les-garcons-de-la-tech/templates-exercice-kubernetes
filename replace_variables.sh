712a509e608080b0a976f50428021cd742c55dac
herve <rvfontbonne@gmail.com>
2024-01-13 15:38:51 +0100

    clean script / improve index html

--- replace_variables.sh
+++ replace_variables.sh
@@ -4,12 +4,11 @@ NUMBER=
 DOMAIN_NAME=formation.fgtech.fr
 PROTOCOLE=https
 IP_NODE=_IP_NODE_
-EXTERNAL_IP_SVC_BACKEND=_EXTERNAL_IP_SVC_BACKEND_
+
 
 
 find . -type f -name "*.yaml" | xargs sed -i  "s/_NUMBER_/$NUMBER/g;"
 find . -type f -name "*.yaml" | xargs sed -i  "s/_IP_NODE_/$IP_NODE/g;"
-find . -type f -name "*.yaml" | xargs sed -i  "s/_EXTERNAL_IP_SVC_BACKEND_/$EXTERNAL_IP_SVC_BACKEND/g;"
 find . -type f -name "*.yaml" | xargs sed -i  "s/_DOMAIN_NAME_/$DOMAIN_NAME/g;"
 find . -type f -name "*.yaml" | xargs sed -i  "s/_PROTOCOLE_/$PROTOCOLE/g;"
 
